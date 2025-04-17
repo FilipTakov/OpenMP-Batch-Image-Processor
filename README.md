6c920a3b150d78b648d285ad99e302d070f68a39
FilipTakov <filip.takov@ontariotechu.net>
2025-04-17 03:08:52 -0400

    Final Submission
    updated output to be clearer

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 # USAGE:
 
-Place images into "image_input" folder.
+Place images into "image_input" folder. If another folder is to be desired, change the INPUT_FOLDER value in main0.c. This also applies to "image_output" and thread count.
 
 Compile using: gcc -std=c17 -Wall main0.c -o main0.o -lm
 
@@ -12,9 +12,43 @@ Run while listing all image file names to be used as arguments: ./main0.o exampl
 
 Choose any image operation you would like using the terminal instructions: "gs", "sp", "hf", "vf".
 
-Retyping the operation will deselect it. Note that greyscale and sepia are mutually exclusive.
+Retyping the operation will deselect it. Note that greyscale and sepia are mutually exclusive, and selecting one deselects the other.
+
+Type "confirm" to proceed.
+
+Once done, the images will be in the output folder "image_output" if the defined variable was not changed.
 
 
-# Features Implemented:
+# Operations:
 
-Supports OpenMP parallelization.
+Greyscale: Turns an image black and white, reducing the channel count to 1, or 2 if an alpha channel (transparency) exists.
+
+Sepia: Applies a sepia filter over an image.
+
+Horizontal Flip: Horizontally mirrors an image.
+
+Vertical Flip: Vertically mirrors an image.
+
+# Changing Defined Variables
+
+Some defined variables at the top of main0.c can be changed to support the user's needs.
+
+"INPUT_FOLDER" can be changed to let users use a different folder to place their images.
+
+"OUTPUT_FOLDER" can be changed to let users use a different folder to output their images.
+
+"NUM_THREADS" can be changed to adjust the number of threads the image processor uses.
+
+
+# Sample Images:
+
+Some sample images have been provided to let users try the processor.
+
+large_image.jpg and large_image2.jpg are larger files that spotlight the benefits of parallelization, processing and outputting.
+
+tighten_smug.png and rdj.png test png compatibility, and also rdj.png contains an alpha channel, so transparency can be tested.
+
+man.jpg can be used to compare against the larger images due to its smaller file size.
+
+
+
--- main0.c
+++ main0.c
@@ -8,19 +8,28 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image/stb_image_write.h"
 
+//folder locations
 #define INPUT_FOLDER "image_input/"
 #define OUTPUT_FOLDER "image_output/"
 
+//thread count
+#define NUM_THREADS 16
 
-//returns file extension
+
+//1 thread results in serialization
+//n threads where n is the number of images results in each image being worked on but without processing speedups until some threads finish their image while others are still working.
+//>n threads results in right away processing speedups for as many extra threads exist.
+
+
+//returns file extension by finding last "." in a string
 char* get_filename_ext(char* filename) {
     char* dot = strrchr(filename, '.');
     return (!dot || dot == filename) ? "" : dot + 1;
 }
 
-//Grayscale operation takes average of rgb into a single channel
+//Grayscale operation takes average of rgb values into a single channel
 void apply_grayscale(unsigned char* img, unsigned char* output_img, int width, int height, int channels, int output_channels) {
-#pragma omp parallel for
+#pragma omp taskloop
     for (size_t i = 0; i < width * height; ++i) {
         unsigned char* p = img + i * channels;
         unsigned char* pg = output_img + i * output_channels;
@@ -49,6 +58,7 @@ void apply_hflip(unsigned char* img, int width, int height, int channels) {
         for (int x = 0; x < width / 2; x++) {
             int left = (y * width + x) * channels;
             int right = (y * width + (width - x - 1)) * channels;
+            //go through existing channels
             for (int c = 0; c < channels; c++) {
                 unsigned char tmp = img[left + c];
                 img[left + c] = img[right + c];
@@ -66,21 +76,26 @@ void apply_vflip(unsigned char* img, int width, int height, int channels) {
         int src_y = height - y - 1;
         memcpy(flipped + y * width * channels, img + src_y * width * channels, width * channels);
     }
+    //move flipped to img
     memcpy(img, flipped, width * height * channels);
     free(flipped);
 }
 
 
 int main(int argc, char* argv[]) {
+    //Input Validation: filenames are provided.
     if (argc < 2) {
         printf("Error: Provide image filenames.\n");
         return 0;
     }
-
-    int greyscale = 0, hflip = 0, vflip = 0, sepia = 0;
-    char input[8];
+    
+    //input sequence
+    char input[16];
     printf("Select operations (\"confirm\" to proceed):\nNote: Greyscale and Sepia are mutually exclusive.\nGreyscale: \"gs\", Sepia: \"sp\", Horizontal Flip: \"hf\", Vertical Flip: \"vf\" \n");
     
+    //Operation bools
+    int greyscale = 0, hflip = 0, vflip = 0, sepia = 0;
+
     //While input not "confirm", modify operation values
     while (scanf("%s", input) && strcmp(input, "confirm") != 0) {
         if (strcmp(input, "gs") == 0) {
@@ -97,15 +112,14 @@ int main(int argc, char* argv[]) {
         printf("Chosen: gs(%d), sp(%d), hf(%d), vf(%d)\n", greyscale, sepia, hflip, vflip);
     }
 
-    //set threads
-    int num_threads = 32;
-    omp_set_num_threads(num_threads);
-
-    //image parallelization timing
-
+    //start total timer after input
     double input_start = omp_get_wtime();
 
-    //each image is an iteration
+
+
+
+    //each image is an iteration, if an image or pointer is unavailable, proceed to next image
+    omp_set_num_threads(NUM_THREADS);
 #pragma omp parallel for
     for (int i = 1; i < argc; i++) {
         int threadId = omp_get_thread_num();
@@ -113,22 +127,22 @@ int main(int argc, char* argv[]) {
         //get path for given image
         char path[256];
         snprintf(path, sizeof(path), "%s%s", INPUT_FOLDER, argv[i]);
-
-
        
         //load image
         int width, height, channels;
+        printf("(%d): loading (%s)...\n", omp_get_thread_num(), argv[i]);
         unsigned char* img = stbi_load(path, &width, &height, &channels, 0);
         if (!img) {
             printf("(%d): Failed to load %s\n", threadId, argv[i]);
             continue;
         }
-
+        size_t img_size = width * height * channels;
+        //create output image variables
         unsigned char* output_img = img;
         int output_channels = channels;
-        size_t img_size = width * height * channels;
 
 
+        printf("(%d): \tLOADED (%s), processing...\n", omp_get_thread_num(), argv[i]);
         //Processing Timing
         double start; double end;
         start = omp_get_wtime();
@@ -153,20 +167,23 @@ int main(int argc, char* argv[]) {
 
         //Processing Timer End
         end = omp_get_wtime();
-        printf("(%d): Processing took %f seconds\n", omp_get_thread_num(), end - start);
 
+
+
+        //get output file path
         char out_path[256];
         snprintf(out_path, sizeof(out_path), "%s%s", OUTPUT_FOLDER, argv[i]);
         char* ext = get_filename_ext(argv[i]);
         
         //Writing to correct filetype (PNG and JPG supported)
-        printf("(%d): Writing to (%s)\n", omp_get_thread_num(), out_path);
+        printf("(%d): \t\tPROCESSED in %f seconds, Writing: (%s)...\n", omp_get_thread_num(), end - start, argv[i]);
         if (strcmp(ext, "png") == 0) {
             stbi_write_png(out_path, width, height, output_channels, output_img, width * output_channels);
         }
         else if (strcmp(ext, "jpg") == 0 || strcmp(ext, "jpeg") == 0) {
             stbi_write_jpg(out_path, width, height, output_channels, output_img, 100);
         }
+        printf("(%d): \t\t\tWRITTEN: (%s)\n", omp_get_thread_num(), out_path);
 
         stbi_image_free(img);
         if (output_img != img) free(output_img);
@@ -175,6 +192,6 @@ int main(int argc, char* argv[]) {
     //Mark completion time
     double input_end = omp_get_wtime();
 
-    printf("Completed all images in %f seconds using %d threads\n", input_end - input_start, num_threads);
+    printf("Completed all images in %f seconds using %d threads\n", input_end - input_start, NUM_THREADS);
     return 0;
 }
